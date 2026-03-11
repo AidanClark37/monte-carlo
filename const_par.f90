1aa379081155b992e6e8082efa115565ab6bb0a4
aidan <acoar086@odu.edu>
2026-03-10 22:19:10 -0400

    checkpoint got f workign

---
+++ const_par.f90
@@ -0,0 +1,24 @@
+module constant_par
+  implicit none
+
+  !costants                                                                                                             
+
+
+
+
+  real*8,parameter::hbarc  =197.32698d0
+  real*8,parameter::m      =138.03919333333d0 
+
+
+
+
+
+  real*8,parameter::pi=acos(-1.d0)
+
+
+
+
+
+
+end module constant_par
+
